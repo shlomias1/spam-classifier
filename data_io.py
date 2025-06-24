6abc12993f7ec1e5d3f6047e10b03de3930ec28f
Shlomi Assayag <112924575+shlomias1@users.noreply.github.com>
2025-06-24 06:59:31 +0300

    Update data_io.py

--- data_io.py
+++ data_io.py
@@ -2,8 +2,13 @@ import pandas as pd
 import numpy as np
 import pickle
 
-spam_data = pd.read_csv('data/spam.csv', encoding='latin-1')
+def load_data(file_path):
+    data = pd.read_csv(file_path, encoding='latin-1')
+    return data
 
+def save_to_csv(data, filename='data/spam_processing_data.csv'):
+    data.to_csv(filename, index=False)
+    
 def save_processed_data(X_train, X_test, y_train, y_test, tfidf_vectorizer, filename_prefix='processed_data'):
     np.savez_compressed(f'{filename_prefix}_train.npz', X=X_train, y=y_train)
     np.savez_compressed(f'{filename_prefix}_test.npz', X=X_test, y=y_test)
