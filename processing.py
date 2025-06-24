19419faeb099ab423a28fefd1dfea44958ffffac
Shlomi Assayag <112924575+shlomias1@users.noreply.github.com>
2025-06-24 07:00:51 +0300

    Update processing.py

--- processing.py
+++ processing.py
@@ -1,6 +1,8 @@
 from sklearn.feature_extraction.text import TfidfVectorizer
 from sklearn.model_selection import train_test_split
 from scipy.sparse import hstack
+import numpy as np
+
 import config
 
 # TF-IDF Vectorization for textual input
@@ -10,7 +12,7 @@ def tf_idf_vectorization(data):
     # Fit and transform the cleaned text data
     X_tfidf = tfidf.fit_transform(data['cleaned'])
     # Features to use for tree-based model
-    X_manual = data[config.SELECTED_FEATURES].values
+    X_manual = data[config.SELECTED_FEATURES].astype(float).values
     # Final design matrix
     X_all = hstack([X_tfidf, X_manual])
     return X_all, tfidf
@@ -20,3 +22,9 @@ def split(data, X_all):
     y = data['label'].map({'ham': 0, 'spam': 1})
     X_train, X_test, y_train, y_test = train_test_split(X_all, y, stratify=y, test_size=0.2, random_state=42)
     return X_train, X_test, y_train, y_test
+
+def to_ndarray(X):
+    if not isinstance(X, np.ndarray):
+        return X.toarray()
+    else:
+        return X 
